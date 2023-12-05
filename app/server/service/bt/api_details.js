eb9a224485eb7c36de3c8c57ff977c8fe6271fe8
Andrei Manolache <andreim98@yahoo.com>
2023-12-05 15:50:29 +0200

    removed secrets

---
+++ app/server/service/bt/api_details.js
@@ -0,0 +1,11 @@
+const details = {
+    ENDPOINT: 'https://api.apistorebt.ro/bt/sb/',
+    BT_CLIENT_ID: 'CLIENT_ID',
+    BT_CLIENT_SECRET: 'CLIENT_SECRET',
+    REDIRECT_URI: 'https://callback',
+    BT_REGISTRATION_ACCESS_TOKEN: 'HARDCODED_ACCESS_TOKEN'
+};
+
+module.exports = {
+    details
+};
